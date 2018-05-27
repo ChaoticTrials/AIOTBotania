08ba9b964fcfbe22ad13757dd79f2f947b50a420
derkalaender <derkalaender@gmx.de>
2018-05-27 10:16:35 +0200

    Move hoe code to base class

--- src/main/java/de/melanx/aiotbotania/items/base/ItemHoeBase.java
+++ src/main/java/de/melanx/aiotbotania/items/base/ItemHoeBase.java
@@ -1,13 +1,49 @@
 package de.melanx.aiotbotania.items.base;
 
 import de.melanx.aiotbotania.items.ModItems;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemHoe;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import vazkii.botania.api.mana.IManaUsingItem;
+import vazkii.botania.api.mana.ManaItemHandler;
+import vazkii.botania.common.item.equipment.tool.ToolCommons;
 
-public class ItemHoeBase extends ItemHoe {
+import javax.annotation.Nonnull;
 
-    public ItemHoeBase(String name, ToolMaterial mat) {
+public class ItemHoeBase extends ItemHoe implements IManaUsingItem {
+
+    private int MANA_PER_DAMAGE;
+
+    public ItemHoeBase(String name, ToolMaterial mat, int MANA_PER_DAMAGE) {
         super(mat);
         ModItems.registerItem(this, name);
         ModItems.registerModel(this);
+
+        this.MANA_PER_DAMAGE = MANA_PER_DAMAGE;
+    }
+
+    @Override
+    public void onUpdate(ItemStack stack, World world, Entity player, int par4, boolean par5) {
+        if(!world.isRemote && player instanceof EntityPlayer && stack.getItemDamage() > 0 && ManaItemHandler.requestManaExactForTool(stack, (EntityPlayer) player, MANA_PER_DAMAGE * 2, true))
+            stack.setItemDamage(stack.getItemDamage() - 1);
+    }
+
+    @Override
+    public boolean getIsRepairable(ItemStack par1ItemStack, @Nonnull ItemStack par2ItemStack) {
+        return par2ItemStack.getItem() == vazkii.botania.common.item.ModItems.manaResource && par2ItemStack.getItemDamage() == 0 || super.getIsRepairable(par1ItemStack, par2ItemStack);
+    }
+
+    @Override
+    public boolean hitEntity(ItemStack par1ItemStack, EntityLivingBase par2EntityLivingBase, @Nonnull EntityLivingBase par3EntityLivingBase) {
+        ToolCommons.damageItem(par1ItemStack, 1, par3EntityLivingBase, MANA_PER_DAMAGE);
+        return true;
+    }
+
+    @Override
+    public boolean usesMana(ItemStack itemStack) {
+        return true;
     }
 }
--- src/main/java/de/melanx/aiotbotania/items/manasteel/ItemManasteelHoe.java
+++ src/main/java/de/melanx/aiotbotania/items/manasteel/ItemManasteelHoe.java
@@ -1,63 +1,13 @@
 package de.melanx.aiotbotania.items.manasteel;
 
 import de.melanx.aiotbotania.items.base.ItemHoeBase;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.SoundEvents;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import vazkii.botania.api.BotaniaAPI;
-import vazkii.botania.api.mana.IManaUsingItem;
-import vazkii.botania.api.mana.ManaItemHandler;
-import vazkii.botania.common.item.equipment.tool.ToolCommons;
 
-import javax.annotation.Nonnull;
+public class ItemManasteelHoe extends ItemHoeBase {
 
-public class ItemManasteelHoe extends ItemHoeBase implements IManaUsingItem {
-
-    private static final int MANA_PER_DAMAGE = 60;
+    private static final int MANA_PER_DAMAGE = 1000;
 
     public ItemManasteelHoe() {
-        super( "manasteelHoe", BotaniaAPI.manasteelToolMaterial);
-    }
-
-    @Override
-    public boolean hitEntity(ItemStack par1ItemStack, EntityLivingBase par2EntityLivingBase, @Nonnull EntityLivingBase par3EntityLivingBase) {
-        ToolCommons.damageItem(par1ItemStack, 1, par3EntityLivingBase, getManaPerDmg());
-        return true;
-    }
-
-    @Override
-    protected void setBlock(ItemStack stack, EntityPlayer player, World worldIn, BlockPos pos, IBlockState state) {
-        worldIn.playSound(player, pos, SoundEvents.ITEM_HOE_TILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
-
-        if(!worldIn.isRemote) {
-            worldIn.setBlockState(pos, state, 11);
-            stack.damageItem(1, player);
-        }
-    }
-
-    public int getManaPerDmg() {
-        return MANA_PER_DAMAGE;
-    }
-
-    @Override
-    public void onUpdate(ItemStack stack, World world, Entity player, int par4, boolean par5) {
-        if(!world.isRemote && player instanceof EntityPlayer && stack.getItemDamage() > 0 && ManaItemHandler.requestManaExactForTool(stack, (EntityPlayer) player, MANA_PER_DAMAGE * 2, true))
-            stack.setItemDamage(stack.getItemDamage() - 1);
-    }
-
-    @Override
-    public boolean getIsRepairable(ItemStack par1ItemStack, @Nonnull ItemStack par2ItemStack) {
-        return par2ItemStack.getItem() == vazkii.botania.common.item.ModItems.manaResource && par2ItemStack.getItemDamage() == 0 ? true : super.getIsRepairable(par1ItemStack, par2ItemStack);
-    }
-
-    @Override
-    public boolean usesMana(ItemStack stack) {
-        return true;
+        super( "manasteelHoe", BotaniaAPI.manasteelToolMaterial, MANA_PER_DAMAGE);
     }
 }
