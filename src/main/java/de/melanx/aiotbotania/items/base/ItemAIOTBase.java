6daf3cda04cafe28e6009eef95f0ef39c61ee1a7
MelanX <meissi97@me.com>
2018-05-27 11:20:10 +0200

    We should take bikini bottom and push it somewhere else!

--- src/main/java/de/melanx/aiotbotania/items/base/ItemAIOTBase.java
+++ src/main/java/de/melanx/aiotbotania/items/base/ItemAIOTBase.java
@@ -1,15 +1,80 @@
 package de.melanx.aiotbotania.items.base;
 
+import com.google.common.collect.Sets;
 import de.melanx.aiotbotania.items.ModItems;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemTool;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import vazkii.botania.api.mana.IManaUsingItem;
+import vazkii.botania.api.mana.ManaItemHandler;
+import vazkii.botania.common.item.equipment.tool.ToolCommons;
 
+import javax.annotation.Nonnull;
 import java.util.HashSet;
+import java.util.Set;
 
-public class ItemAIOTBase extends ItemTool {
+public class ItemAIOTBase extends ItemTool implements IManaUsingItem {
 
-    public ItemAIOTBase(String name, ToolMaterial mat, float attackDamage, float attackSpeed) {
+    private int MANA_PER_DAMAGE;
+
+    public ItemAIOTBase(String name, ToolMaterial mat, float attackDamage, float attackSpeed, int MANA_PER_DAMAGE) {
         super(attackDamage, attackSpeed, mat, new HashSet<>());
         ModItems.registerItem(this, name);
         ModItems.registerModel(this);
+
+        this.MANA_PER_DAMAGE = MANA_PER_DAMAGE;
+    }
+
+    @Override
+    public boolean hitEntity(ItemStack par1ItemStack, EntityLivingBase par2EntityLivingBase, @Nonnull EntityLivingBase par3EntityLivingBase) {
+        ToolCommons.damageItem(par1ItemStack, 1, par3EntityLivingBase, MANA_PER_DAMAGE);
+        return true;
+    }
+
+    @Override
+    public float getDestroySpeed(ItemStack stack, IBlockState state){
+        if(state.getBlock() == Blocks.WEB){
+            return 15.0F;
+        }
+        else{
+            return state.getBlock().getHarvestTool(state) == null || state.getBlock().getHarvestTool(state).isEmpty() || this.getToolClasses(stack).contains(state.getBlock().getHarvestTool(state)) ? this.efficiency : 1.0F;
+        }
+    }
+
+    @Override
+    public EnumActionResult onItemUse(EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ){
+        if(!playerIn.isSneaking()) return Items.IRON_HOE.onItemUse(playerIn, worldIn, pos, hand, side, hitX, hitY, hitZ);
+        return Items.IRON_SHOVEL.onItemUse(playerIn, worldIn, pos, hand, side, hitX, hitY, hitZ);
+    }
+
+    @Override
+    public Set<String> getToolClasses(ItemStack stack){
+        return Sets.newHashSet("pickaxe", "axe", "shovel");
+    }
+
+    @Override
+    public void onUpdate(ItemStack stack, World world, Entity player, int par4, boolean par5) {
+        if(!world.isRemote && player instanceof EntityPlayer && stack.getItemDamage() > 0 && ManaItemHandler.requestManaExactForTool(stack, (EntityPlayer) player, MANA_PER_DAMAGE * 2, true))
+            stack.setItemDamage(stack.getItemDamage() - 1);
+    }
+
+    @Override
+    public boolean getIsRepairable(ItemStack par1ItemStack, @Nonnull ItemStack par2ItemStack) {
+        return par2ItemStack.getItem() == vazkii.botania.common.item.ModItems.manaResource && par2ItemStack.getItemDamage() == 0 ? true : super.getIsRepairable(par1ItemStack, par2ItemStack);
+    }
+
+    @Override
+    public boolean usesMana(ItemStack itemStack) {
+        return true;
     }
 }
