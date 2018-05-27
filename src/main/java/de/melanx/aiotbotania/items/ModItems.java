4e858b5e58131186a608df71ef030c931e56b7c6
MelanX <meissi97@me.com>
2018-05-27 17:18:54 +0200

    added Elementium AIOT

--- src/main/java/de/melanx/aiotbotania/items/ModItems.java
+++ src/main/java/de/melanx/aiotbotania/items/ModItems.java
@@ -1,6 +1,7 @@
 package de.melanx.aiotbotania.items;
 
 import de.melanx.aiotbotania.AIOTBotania;
+import de.melanx.aiotbotania.items.elementium.ItemElementiumAIOT;
 import de.melanx.aiotbotania.items.elementium.ItemElementiumHoe;
 import de.melanx.aiotbotania.items.manasteel.ItemManasteelAIOT;
 import de.melanx.aiotbotania.items.manasteel.ItemManasteelHoe;
@@ -18,12 +19,14 @@ public class ModItems {
     public static Item manaaiot;
 
     public static Item elementiumhoe;
+    public static Item elementiumaiot;
 
     public static void init() {
         manahoe = new ItemManasteelHoe();
         manaaiot = new ItemManasteelAIOT();
 
         elementiumhoe = new ItemElementiumHoe();
+        elementiumaiot = new ItemElementiumAIOT();
     }
 
     public static void registerItem(Item item, String name) {
