745c6fdd1da2e748c6e658afae226a898b3a8160
MelanX <meissi97@me.com>
2018-07-23 14:36:12 +0200

    Axe added

--- src/main/java/de/melanx/aiotbotania/items/elementium/ItemElementiumAIOT.java
+++ src/main/java/de/melanx/aiotbotania/items/elementium/ItemElementiumAIOT.java
@@ -5,16 +5,28 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockFalling;
 import net.minecraft.block.material.Material;
 import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.monster.*;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Enchantments;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import vazkii.botania.api.BotaniaAPI;
 import vazkii.botania.api.item.IPixieSpawner;
+import vazkii.botania.common.core.helper.ItemNBTHelper;
+import vazkii.botania.common.entity.EntityDoppleganger;
+import vazkii.botania.common.item.ModItems;
 import vazkii.botania.common.item.equipment.tool.ToolCommons;
+
+import java.util.Random;
 
 import static vazkii.botania.common.item.equipment.tool.ToolCommons.materialsShovel;
 
@@ -24,6 +36,7 @@ public class ItemElementiumAIOT extends ItemAIOTBase implements IPixieSpawner {
 
     public ItemElementiumAIOT() {
         super("elementiumAIOT", BotaniaAPI.elementiumToolMaterial, 6.0f, -2.2f, MANA_PER_DAMAGE, true);
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
     // The following code is by Vazkii
@@ -53,4 +66,35 @@ public class ItemElementiumAIOT extends ItemAIOTBase implements IPixieSpawner {
 
         return false;
     }
+
+    @SubscribeEvent
+    public void onEntityDrops(LivingDropsEvent event) {
+        if(event.isRecentlyHit() && event.getSource().getTrueSource() != null && event.getSource().getTrueSource() instanceof EntityPlayer) {
+            ItemStack weapon = ((EntityPlayer) event.getSource().getTrueSource()).getHeldItemMainhand();
+            if(!weapon.isEmpty() && weapon.getItem() == this) {
+                Random rand = event.getEntityLiving().world.rand;
+                int looting = EnchantmentHelper.getEnchantmentLevel(Enchantments.FORTUNE, weapon);
+
+                if(event.getEntityLiving() instanceof AbstractSkeleton && rand.nextInt(1) <= 3 + looting)
+                    addDrop(event, new ItemStack(Items.SKULL, 1, event.getEntityLiving() instanceof EntityWitherSkeleton ? 1 : 0));
+                else if(event.getEntityLiving() instanceof EntityZombie && !(event.getEntityLiving() instanceof EntityPigZombie) && rand.nextInt(26) <= 2 + 2 * looting)
+                    addDrop(event, new ItemStack(Items.SKULL, 1, 2));
+                else if(event.getEntityLiving() instanceof EntityCreeper && rand.nextInt(26) <= 2 + 2 * looting)
+                    addDrop(event, new ItemStack(Items.SKULL, 1, 4));
+                else if(event.getEntityLiving() instanceof EntityPlayer && rand.nextInt(11) <= 1 + looting) {
+                    ItemStack stack = new ItemStack(Items.SKULL, 1, 3);
+                    ItemNBTHelper.setString(stack, "SkullOwner", event.getEntityLiving().getName());
+                    addDrop(event, stack);
+                } else if(event.getEntityLiving() instanceof EntityDoppleganger && rand.nextInt(13) < 1 + looting)
+                    addDrop(event, new ItemStack(ModItems.gaiaHead));
+            }
+        }
+    }
+
+    private void addDrop(LivingDropsEvent event, ItemStack drop) {
+        EntityItem entityitem = new EntityItem(event.getEntityLiving().world, event.getEntityLiving().posX, event.getEntityLiving().posY, event.getEntityLiving().posZ, drop);
+        entityitem.setPickupDelay(10);
+        event.getDrops().add(entityitem);
+    }
+
 }
