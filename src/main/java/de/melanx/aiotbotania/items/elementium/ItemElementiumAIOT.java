3d52d519b4cf6e50b9e9a70f137f540b5663fc0b
MelanX <meissi97@me.com>
2018-07-23 14:51:26 +0200

    Pickaxe added

--- src/main/java/de/melanx/aiotbotania/items/elementium/ItemElementiumAIOT.java
+++ src/main/java/de/melanx/aiotbotania/items/elementium/ItemElementiumAIOT.java
@@ -17,8 +17,10 @@ import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.oredict.OreDictionary;
 import vazkii.botania.api.BotaniaAPI;
 import vazkii.botania.api.item.IPixieSpawner;
 import vazkii.botania.common.core.helper.ItemNBTHelper;
@@ -39,7 +41,7 @@ public class ItemElementiumAIOT extends ItemAIOTBase implements IPixieSpawner {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    // The following code is by Vazkii
+    // The following code is by Vazkii (https://github.com/Vazkii/Botania/tree/master/src/main/java/vazkii/botania/common/item/equipment/tool/elementium/ <-- Axe, Pick, Shovel and Sword)
 
     @Override
     public float getPixieChance(ItemStack stack) {
@@ -95,6 +97,42 @@ public class ItemElementiumAIOT extends ItemAIOTBase implements IPixieSpawner {
         EntityItem entityitem = new EntityItem(event.getEntityLiving().world, event.getEntityLiving().posX, event.getEntityLiving().posY, event.getEntityLiving().posZ, drop);
         entityitem.setPickupDelay(10);
         event.getDrops().add(entityitem);
+    }
+
+    @SubscribeEvent
+    public void onHarvestDrops(BlockEvent.HarvestDropsEvent event) {
+        if(event.getHarvester() != null) {
+            ItemStack stack = event.getHarvester().getHeldItemMainhand();
+            if(!stack.isEmpty() && (stack.getItem() == this)) {
+                event.getDrops().removeIf(s -> !s.isEmpty() && (isDisposable(s)
+                        || isSemiDisposable(s) && !event.getHarvester().isSneaking()));
+            }
+        }
+    }
+
+    public static boolean isDisposable(Block block) {
+        return isDisposable(new ItemStack(block));
+    }
+
+    private static boolean isDisposable(ItemStack stack) {
+        if(stack.isEmpty())
+            return false;
+
+        for(int id : OreDictionary.getOreIDs(stack)) {
+            String name = OreDictionary.getOreName(id);
+            if(BotaniaAPI.disposableBlocks.contains(name))
+                return true;
+        }
+        return false;
+    }
+
+    private static boolean isSemiDisposable(ItemStack stack) {
+        for(int id : OreDictionary.getOreIDs(stack)) {
+            String name = OreDictionary.getOreName(id);
+            if(BotaniaAPI.semiDisposableBlocks.contains(name))
+                return true;
+        }
+        return false;
     }
 
 }
