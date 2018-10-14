eeb80cd25ae8c45b9f4992bf697e3c7bdb4403ca
MelanX <meissi97@me.com>
2018-10-14 16:26:59 +0200

    fixed creativetab icon
    there was no icon when aiots turned off in config

--- src/main/java/de/melanx/aiotbotania/client/aiotbotaniaTab.java
+++ src/main/java/de/melanx/aiotbotania/client/aiotbotaniaTab.java
@@ -1,5 +1,6 @@
 package de.melanx.aiotbotania.client;
 
+import de.melanx.aiotbotania.config.values.ConfigBoolValues;
 import de.melanx.aiotbotania.items.ModItems;
 import de.melanx.aiotbotania.lib.LibMisc;
 import net.minecraft.creativetab.CreativeTabs;
@@ -15,7 +16,15 @@ public class aiotbotaniaTab extends CreativeTabs {
 
     @Override
     public ItemStack getTabIconItem() {
-        return new ItemStack(ModItems.manaaiot);
+        if (ConfigBoolValues.MANASTEEL_AIOT.isEnabled())
+            return new ItemStack(ModItems.manaaiot);
+        if (!ConfigBoolValues.MANASTEEL_AIOT.isEnabled()) {
+            if (ConfigBoolValues.ELEMENTIUM_AIOT.isEnabled())
+                return new ItemStack(ModItems.elementiumaiot);
+            else
+                return new ItemStack(ModItems.elementiumhoe);
+        }
+        return getTabIconItem();
     }
 
 }
