66cf758c02535ec22ca062019cfc2ad3c936675b
MelanX <meissi97@me.com>
2019-12-29 23:51:45 +0100

    changed the registration for config reasons

--- src/main/java/de/melanx/aiotbotania/core/Registration.java
+++ src/main/java/de/melanx/aiotbotania/core/Registration.java
@@ -2,6 +2,7 @@ package de.melanx.aiotbotania.core;
 
 import de.melanx.aiotbotania.AIOTBotania;
 import de.melanx.aiotbotania.blocks.BlockCustomFarmland;
+import de.melanx.aiotbotania.core.config.ConfigHandler;
 import de.melanx.aiotbotania.items.ItemTiers;
 import de.melanx.aiotbotania.items.base.ItemSwordBase;
 import de.melanx.aiotbotania.items.elementium.ItemElementiumAIOT;
@@ -16,53 +17,50 @@ import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.IForgeRegistry;
 
 @Mod.EventBusSubscriber(modid = AIOTBotania.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Registration {
     @SubscribeEvent
     public static void registerItems(final RegistryEvent.Register<Item> e) {
-        e.getRegistry().registerAll(
-                // AIOTs
-                new ItemElementiumAIOT(),
-                new ItemManasteelAIOT(),
-                new ItemLivingrockAIOT(),
-                new ItemLivingwoodAIOT(),
+        IForgeRegistry<Item> registry = e.getRegistry();
 
-                // Axes
-                new ItemLivingrockAxe(),
-                new ItemLivingwoodAxe(),
+        // Livingwood
+        if (ConfigHandler.COMMON.LIVINGWOOD_TOOLS.get()) {
+            registry.register(new ItemLivingwoodShears());
+            registry.register(new ItemSwordBase("livingwood_sword", ItemTiers.LIVINGROCK_ITEM_TIER, 3, -2.5F, 30));
+            registry.register(new ItemLivingwoodAxe());
+            registry.register(new ItemLivingwoodPickaxe());
+            registry.register(new ItemLivingwoodShovel());
+            registry.register(new ItemLivingwoodHoe());
+            registry.register(new ItemLivingwoodAIOT());
+        }
 
-                // Hoes
-                new ItemElementiumHoe(),
-                new ItemLivingrockHoe(),
-                new ItemLivingwoodHoe(),
-                new ItemManasteelHoe(),
+        // Livingrock
+        if (ConfigHandler.COMMON.LIVINGROCK_TOOLS.get()) {
+            registry.register(new ItemLivingrockShears());
+            registry.register(new ItemSwordBase("livingrock_sword", ItemTiers.LIVINGROCK_ITEM_TIER, 2, -2.4F, 40));
+            registry.register(new ItemLivingrockAxe());
+            registry.register(new ItemLivingrockPickaxe());
+            registry.register(new ItemLivingrockShovel());
+            registry.register(new ItemLivingrockHoe());
+            registry.register(new ItemLivingrockAIOT());
+        }
 
-                // Pickaxes
-                new ItemLivingrockPickaxe(),
-                new ItemLivingwoodPickaxe(),
+        // Manasteel
+        registry.register(new ItemManasteelHoe());
+        registry.register(new ItemManasteelAIOT());
 
-                // Shears
-                new ItemLivingrockShovel(),
-                new ItemLivingwoodShears(),
-
-                // Shovels
-                new ItemLivingrockShears(),
-                new ItemLivingwoodShovel(),
-
-                // Swords
-                new ItemSwordBase("livingrock_sword", ItemTiers.LIVINGROCK_ITEM_TIER, 2, -2.4F, 40),
-                new ItemSwordBase("livingwood_sword", ItemTiers.LIVINGROCK_ITEM_TIER, 3, -2.5F, 30)
-        );
+        // Elementium
+        registry.register(new ItemElementiumHoe());
+        registry.register(new ItemElementiumAIOT());
 
         AIOTBotania.instance.getLogger().info("Items registered.");
     }
 
     @SubscribeEvent
     public static void registerBlocks(final RegistryEvent.Register<Block> e) {
-        e.getRegistry().registerAll(
-                new BlockCustomFarmland(Block.Properties.from(Blocks.FARMLAND))
-        );
+        e.getRegistry().register(new BlockCustomFarmland(Block.Properties.from(Blocks.FARMLAND)));
 
         AIOTBotania.instance.getLogger().info("Blocks registered.");
     }
