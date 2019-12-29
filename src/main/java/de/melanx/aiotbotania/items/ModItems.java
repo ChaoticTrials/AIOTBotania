3bc13865f4540a80ec8db2b0ae8dde814db30cd5
MelanX <meissi97@me.com>
2019-12-29 23:37:24 +0100

    removed old code

--- src/main/java/de/melanx/aiotbotania/items/ModItems.java
+++ src/main/java/de/melanx/aiotbotania/items/ModItems.java
@@ -1,10 +1,6 @@
 package de.melanx.aiotbotania.items;
 
 import de.melanx.aiotbotania.AIOTBotania;
-import de.melanx.aiotbotania.items.elementium.*;
-import de.melanx.aiotbotania.items.manasteel.*;
-import de.melanx.aiotbotania.items.livingrock.*;
-import de.melanx.aiotbotania.items.livingwood.*;
 import net.minecraft.item.Item;
 import net.minecraftforge.registries.ObjectHolder;
 
@@ -53,33 +49,5 @@ public class ModItems {
     public static Item elementium_hoe;
     @ObjectHolder(AIOTBotania.MODID + ":elementium_aiot")
     public static Item elementium_aiot;
-
-//    public static void init() {
-//        // Livingwood
-//        livingwood_shears = new ItemLivingwoodShears();
-//        livingwood_sword = new ItemLivingwoodSword();
-//        livingwood_axe = new ItemLivingwoodAxe();
-//        livingwood_pickaxe = new ItemLivingwoodPickaxe();
-//        livingwood_shovel = new ItemLivingwoodShovel();
-//        livingwood_hoe = new ItemLivingwoodHoe();
-//        livingwood_aiot = new ItemLivingwoodAIOT();
-//
-//        // Livingrock
-//        livingrock_shears = new ItemLivingrockShears();
-//        livingrock_sword = new ItemLivingrockSword();
-//        livingrock_axe = new ItemLivingrockAxe();
-//        livingrock_pickaxe = new ItemLivingrockPickaxe();
-//        livingrock_shovel = new ItemLivingrockShovel();
-//        livingrock_hoe = new ItemLivingrockHoe();
-//        livingrock_aiot = new ItemLivingrockAIOT();
-//
-//        // Manasteel
-//        manasteel_hoe = new ItemManasteelHoe();
-//        manasteel_aiot = new ItemManasteelAIOT();
-//
-//        // Elementium
-//        elementium_hoe = new ItemElementiumHoe();
-//        elementium_aiot = new ItemElementiumAIOT();
-//    }
 
 }
