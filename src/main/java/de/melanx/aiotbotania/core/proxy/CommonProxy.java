d994f5aaa3f883b594f65e9795996d37d950b44c
MelanX <meissi97@me.com>
2019-12-30 16:09:12 +0100

    delete a lot of unused code

--- src/main/java/de/melanx/aiotbotania/core/proxy/CommonProxy.java
+++ src/main/java/de/melanx/aiotbotania/core/proxy/CommonProxy.java
@@ -1,15 +1,10 @@
 package de.melanx.aiotbotania.core.proxy;
 
-import de.melanx.aiotbotania.capabilities.chunkmarker.ChunkMarker;
-import de.melanx.aiotbotania.capabilities.chunkmarker.ChunkMarkerStorage;
-import de.melanx.aiotbotania.capabilities.farmlanddata.FarmlandData;
-import de.melanx.aiotbotania.capabilities.farmlanddata.FarmlandDataStorage;
 import de.melanx.aiotbotania.core.config.ConfigHandler;
-import net.minecraftforge.common.capabilities.CapabilityManager;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 public class CommonProxy {
@@ -18,15 +13,6 @@ public class CommonProxy {
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ConfigHandler.COMMON_SPEC);
 
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-        registerListeners(bus);
-    }
-
-    public void registerListeners(IEventBus bus) {
-        bus.addListener(this::setup);
-    }
-
-    public void setup(FMLCommonSetupEvent event) {
-        CapabilityManager.INSTANCE.register(ChunkMarker.class, new ChunkMarkerStorage(), ChunkMarker::new);
-        CapabilityManager.INSTANCE.register(FarmlandData.class, new FarmlandDataStorage(), FarmlandData::new);
+        MinecraftForge.EVENT_BUS.register(bus);
     }
 }
