c6f155e0dedb6eafe4418afdeab4a32acb734c34
MelanX <info@melanx.de>
2023-06-29 16:34:54 +0200

    add tools to forge tags for these tools
    closes #76

--- src/main/java/de/melanx/aiotbotania/data/ModTags.java
+++ src/main/java/de/melanx/aiotbotania/data/ModTags.java
@@ -7,7 +7,9 @@ import net.minecraft.data.tags.BlockTagsProvider;
 import net.minecraft.data.tags.ItemTagsProvider;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.ExistingFileHelper;
@@ -39,6 +41,8 @@ public class ModTags {
 
     public static class Items extends ItemTagsProvider {
 
+        public static final TagKey<Item> TOOLS_AIOTS = ItemTags.create(new ResourceLocation("forge", "tools/aiots"));
+
         public Items(DataGenerator generator, BlockTagsProvider blockTagsProvider, @Nullable ExistingFileHelper helper) {
             super(generator, blockTagsProvider, AIOTBotania.MODID, helper);
         }
@@ -46,6 +50,20 @@ public class ModTags {
         @Override
         protected void addTags() {
             this.tag(Tags.Items.SHEARS).add(Registration.livingwood_shears.get(), Registration.livingrock_shears.get());
+            this.tag(Tags.Items.TOOLS_SWORDS).add(Registration.livingwood_sword.get(), Registration.livingrock_sword.get());
+            this.tag(Tags.Items.TOOLS_AXES).add(Registration.livingwood_axe.get(), Registration.livingrock_axe.get());
+            this.tag(Tags.Items.TOOLS_PICKAXES).add(Registration.livingwood_pickaxe.get(), Registration.livingrock_pickaxe.get());
+            this.tag(Tags.Items.TOOLS_SHOVELS).add(Registration.livingwood_shovel.get(), Registration.livingrock_shovel.get(), Registration.terrasteel_shovel.get(), Registration.alfsteel_shovel.get());
+            this.tag(Tags.Items.TOOLS_HOES).add(Registration.livingwood_hoe.get(), Registration.livingrock_hoe.get(), Registration.terrasteel_hoe.get(), Registration.alfsteel_hoe.get());
+            this.tag(TOOLS_AIOTS).add(
+                    Registration.livingwood_aiot.get(),
+                    Registration.livingrock_aiot.get(),
+                    Registration.manasteel_aiot.get(),
+                    Registration.elementium_aiot.get(),
+                    Registration.terrasteel_aiot.get(),
+                    Registration.alfsteel_aiot.get()
+            );
+            this.tag(Tags.Items.TOOLS).addTag(TOOLS_AIOTS);
         }
     }
 }
